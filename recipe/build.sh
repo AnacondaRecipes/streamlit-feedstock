002f194e28c2c8be1a40172894743cc7006e0fe2
Andrii Osipov <aosipov@anaconda.com>
2025-02-10 14:58:17 -0800

    fix win and build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,4 @@ set -ex
 
 protoc --proto_path=proto --python_out=lib proto/streamlit/proto/*.proto
 
-{{ PYTHON }} -m pip install ./lib -vv --no-deps --no-build-isolation
+$PYTHON -m pip install ./lib -vv --no-deps --no-build-isolation
