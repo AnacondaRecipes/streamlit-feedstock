a503a9e40099ea8428fc34997dc19dd06ff7d5fb
Andrii Osipov <aosipov@anaconda.com>
2025-02-10 14:51:54 -0800

    Run script section to the diferent files

---
+++ recipe/bld.bat
@@ -0,0 +1,8 @@
+:: Set the protobuf command
+set PROTOC=protoc
+
+:: Compile the .proto files to Python code
+%PROTOC% --proto_path=proto --python_out=lib proto\streamlit\proto\*.proto
+
+:: Install the generated Python package
+%PYTHON% -m pip install ./lib -vv --no-deps --no-build-isolation
